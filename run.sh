aa72818191387451b370fe5812aee8d706fc15b3
Pedro Manoel Evangelista <pevange@thoughtworks.com>
2017-12-14 16:07:44 -0200

    Autoriza chave ssh se variável de ambiente existir
    Se a variável de ambiente SSH_PUBKEY estiver definida, um arquivo será
    criado em `/home/ssh_user/.ssh/authorized_keys` com o seu valor durante
    a inicialização do container.

--- run.sh
+++ run.sh
@@ -3,4 +3,16 @@
 set -e
 
 service ssh start
+
+if [ -n "$SSH_PUBKEY" ]; then
+  SSH_DIR=/home/ssh_user/.ssh
+  mkdir -p $SSH_DIR
+
+  echo $SSH_PUBKEY >> $SSH_DIR/authorized_keys
+
+  chmod 700 $SSH_DIR
+  chmod 600 $SSH_DIR/authorized_keys
+  chown ssh_user:ssh_user -R $SSH_DIR
+fi
+
 httpd-foreground
